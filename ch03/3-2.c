739b1e46d55af847ca617324e7a23d114f1b27a9
Kim Dongyun <edw0728@gmail.com>
2023-04-14 18:54:12 +0900

    complete 3-2 calc

--- ch03/3-2.c
+++ ch03/3-2.c
@@ -7,15 +7,18 @@ int getlin(char s[], int lim){
     int c, i = 0;
     while (--lim > 0 && (c=getchar()) != EOF && c != '\n')
         s[i++] = c;
+    if (c == '\n')
+        s[i++] = c;
     s[i] = '\0';
     return i;
 }
 
 double atof(char s[]){
-    int val, power, i;
-
+    int val, power, i, sign;
     for (i = 0; isspace(s[i]); i++);
-
+    sign = (s[i] == '-') ? -1 : 1;
+    if (s[i] == '+' || s[i] == '-')
+        i++;
     for (val = 0; isdigit(s[i]); i++)
         val = 10 * val + (s[i] - '0');
     if(s[i] == '.')
@@ -24,30 +27,50 @@ double atof(char s[]){
         val = 10 * val + (s[i] - '0');
         power *= 10;
     }
-    return (double) val / power;
+    return sign * (double)val / power;
 }
 
-//double calc(double a, char op, double b){
-//    switch (op){
-//        case '+':
-//            return a + b;
-//        case '-':
-//            return a - b;
-//        case '*':
-//            return a * b;
-//        case '/':
-//            return a / b;
-//        case '%':
-//            return (int) a % (int) b;
-//        default:
-//            return 0;
-//    }
-//}
+void calc(double a, char op, double b, int type){
+    double result;
+    switch (op){
+        case '+':
+            result = a + b;
+            break;
+        case '-':
+            result = a - b;
+            break;
+        case '*':
+            result = a * b;
+            break;
+        case '/':
+            result = a / b;
+            break;
+        case '%':
+            result = (int) a % (int) b;
+            break;
+        default:
+            printf("error: unknown operator\n");
+            return;
+    }
+    if (type)
+        printf("%f\n", result);
+    else
+        printf("%d\n", (int)result);
+
+}
+
+int find(char s[]){
+    int i;
+    for (i = 0; s[i] != '\0'; i++)
+        if (s[i] == '.')
+            return 1;
+    return 0;
+}
 
 
 int main(){
-    double a, b;
-    char op, line[MAXLINE], temp1[MAXLINE];
+    double a, b, typeA, typeB;
+    char op, line[MAXLINE], temp1[MAXLINE], temp2[MAXLINE];
     int i, j;
     while (getlin(line, MAXLINE) > 0){
         for (i = 0; line[i] != ' '; i++)
@@ -56,12 +79,10 @@ int main(){
         a = atof(temp1);
         op = line[i++];
         for (j = 0; line[i] != '\n'; i++, j++)
-            temp1[j] = line[i];
-
-        temp1[++j] = '\0';
-        b = atof(temp1);
-
-//        printf("%g\n", calc(a, op, b));
+            temp2[j] = line[i];
+        temp2[j] = '\0';
+        b = atof(temp2);
+        calc(a, op, b, find(line));
     }
     return 0;
 }
