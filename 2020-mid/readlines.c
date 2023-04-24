5e3640899123560ea96c689212b8a1ea824ea511
Kim Dongyun <edw0728@gmail.com>
2023-04-24 19:19:15 +0900

    readlines

---
+++ 2020-mid/readlines.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#define MAXLINE 1000
+#define ALLOCSIZE 10000
+static char allocbuf[ALLOCSIZE];
+static char *allocp = allocbuf;
+
+int getlin(char s[], int lim){
+    int c, i = 0;
+    while (--lim > 0 && (c=getchar()) != EOF && c != '\n')
+        s[i++] = c;
+    if (c == '\n')
+        s[i++] = c;
+    s[i] = '\0';
+    return i;
+}
+
+char *alloc(int n){
+    if (allocbuf + ALLOCSIZE - allocp >= n){
+        allocp += n;
+        return allocp - n;
+    } else
+        return 0;
+}
+
+int readlines(char lineptr[][ALLOCSIZE], int maxlines){
+    int len, nlines = 0;
+    char *p, line[MAXLINE];
+    while ((len = getlin(line, MAXLINE)) > 0)
+        if (nlines >= maxlines || (p = alloc(len)) == NULL)
+            return -1;
+        else{
+            line[len-1] = '\0';
+            strcpy(lineptr[nlines++], line);
+//            lineptr[nlines++] = p;
+        }
+    return nlines;
+}
+
+void writelines(char lineptr[][ALLOCSIZE], int nlines){
+    for (int i = 0; i < nlines; ++i) {
+        printf("%s\n", lineptr[i]);
+    }
+}
+
+int main(void){
+    char lineptr[MAXLINE][ALLOCSIZE];
+    int n = readlines(lineptr, MAXLINE);
+    writelines(lineptr, n);
+    return 0;
+}
