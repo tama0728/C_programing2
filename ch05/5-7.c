ff9f0af46e09733915cb0b3aa2128e0d96f88592
tama0728 <edw0728@gmail.com>
2023-05-02 04:38:04 +0000

    Lab05

--- ch05/5-7.c
+++ ch05/5-7.c
@@ -65,9 +65,12 @@ int main(int argc, char *argv[]) {
 
     if (argc > 1 && strcmp(argv[1], "-n") == 0)
         numeric = 1;
+    
 
     if ((nlines = readlines(lineptr, MAXLINES)) >= 0){
         qsort((void **) lineptr, 0, nlines-1, (int (*)(const void *, const void *))(numeric ? numcmp : strcmp));
+        
+        printf("After sort.\n");
         writelines(lineptr, nlines);
         return 0;
     } else{
