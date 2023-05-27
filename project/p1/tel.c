8388424ced82b3d01fa81f7062e618bb7b8ed6ec
Kim Dongyun <edw0728@gmail.com>
2023-05-27 15:18:11 +0900

    Project01

--- project/p1/tel.c
+++ project/p1/tel.c
@@ -7,6 +7,7 @@
 #define data "data.txt"
 #define help "help.txt"
 
+//전화번호부 구조체 링크드 리스트로 구현
 typedef struct Tel{
     char name[30];
     char tel[20];
@@ -14,6 +15,7 @@ typedef struct Tel{
     struct Tel *next;
 }Tel;
 
+//동적 할당 전체 해제
 void allFree(Tel *head) {
     Tel *p = head;
     while (p != NULL) {
@@ -23,8 +25,10 @@ void allFree(Tel *head) {
     }
 }
 
+//리스트에 오름차순으로 추가
 Tel *insert(Tel *head, char *name, char *tel, char *memo) {
     Tel *p = head;
+    //리스트가 비어있을 때
     if (p == NULL) {
         head = (Tel *) malloc(sizeof(Tel));
         strcpy(head->name, name);
@@ -33,6 +37,7 @@ Tel *insert(Tel *head, char *name, char *tel, char *memo) {
         head->next = NULL;
         return head;
     }
+    //리스트가 비어있지 않을 때 오름차순으로 추가
     while (p->next != NULL && strcmp(name, p->next->name) > 0) {
         p = p->next;
     }
@@ -45,6 +50,7 @@ Tel *insert(Tel *head, char *name, char *tel, char *memo) {
     return head;
 }
 
+//data.txt 에서 파일 읽어오기
 Tel *init(Tel *head) {
     FILE *fp = fopen(data, "rt");
     if (fp == NULL) {
@@ -53,6 +59,7 @@ Tel *init(Tel *head) {
     }
     char buf[MAX];
     while (fgets(buf, MAX, fp) != NULL){
+        //파일에서 한 줄씩 읽어서 ':'와 '\n'을 기준으로 문자열을 나누고, 각각 name, tel, memo에 저장
         char name[30] = {0}, tel[20] = {0}, memo[40] = {0}, *ptr = strtok(buf, ":\n");
         int i = 0;
         while (ptr != NULL) {
@@ -65,44 +72,52 @@ Tel *init(Tel *head) {
             ptr = strtok(NULL, ":\n");
             i++;
         }
+        //리스트에 추가
         head = insert(head, name, tel, memo);
     }
     fclose(fp);
     return head;
 }
 
+//key 값을 받아 해당 노드 찾기
 void searchTel(struct Tel *head, char *temp){
     struct Tel *p = head;
     int n = 1;
     while (p != NULL) {
+        //name, tel, memo에 key 값이 포함되어 있으면 출력
         if (strstr(p->name, temp) != NULL || strstr(p->tel, temp) != NULL || strstr(p->memo, temp) != NULL) {
             printf("%d %s %s %s\n",n, p->name, p->tel, p->memo);
             n++;
         }
         p = p->next;
     }
+    //key 값이 없으면 no match found 출력
     if (n == 1)
         printf("no match found.\n");
     else
         printf("match found.\n");
 }
 
+//data.txt에 추가
 void add(char *name, char *tel, char *memo) {
     printf("add? [Y/N]: ");
     int c = getchar();
+    //Y 또는 y가 아니면 add 취소
     if (c != 'Y' && c != 'y') {
         printf("add canceled.\n");
         exit(0);
     }
-
-    FILE *fp = fopen(data, "a");
+    //data.txt의 가장 뒤에 추가
+    FILE *fp = fopen(data, "at");
     fprintf(fp, "%s:%s:%s", name, tel, memo);
     fclose(fp);
     printf("add completed.\n");
 }
 
+//data.txt에 리스트에 있는 내용 저장
 void save(struct Tel *head) {
-    FILE *fp = fopen(data, "w");
+    //data.txt의 내용을 모두 지우고 새로 저장
+    FILE *fp = fopen(data, "wt");
     struct Tel *p = head;
     while (p != NULL) {
         fprintf(fp, "%s:%s:%s\n", p->name, p->tel, p->memo);
@@ -112,19 +127,21 @@ void save(struct Tel *head) {
     printf("save completed.\n");
 }
 
+//리스트에서 삭제 후 data.txt에 저장
 void delete(struct Tel *head, char *key) {
     struct Tel *p = head;
     int n = 1, i = 0, tmep[MAX] = {0};
+    //key 값이 포함된 노드 찾기
     while (p != NULL) {
         if (strstr(p->name, key) != NULL || strstr(p->tel, key) != NULL || strstr(p->memo, key) != NULL) {
             printf("%d %s %s %s\n", n, p->name, p->tel, p->memo);
-            tmep[n-1] = i;
+            tmep[n-1] = i;      //key 값이 포함된 노드의 인덱스 저장
             n++;
         }
         i++;
         p = p->next;
     }
-
+    //key 값이 없으면 no match found 출력
     if (n == 1) {
         printf("no match found.\n");
         allFree(head);
@@ -132,7 +149,7 @@ void delete(struct Tel *head, char *key) {
         }
     printf("which one? ");
     int num = getchar();
-
+    //입력이 숫자가 아니거나 1~n-1 사이의 숫자가 아니면 wrong input 출력
     if (!isdigit(num) || num - '0' > n - 1 || num - '0' < 1) {
         printf("wrong input.\n");
         allFree(head);
@@ -140,24 +157,30 @@ void delete(struct Tel *head, char *key) {
     }
     num = tmep[num - '1'];
     p = head;
+    //삭제할 노드까지 이동
     for (int j = 0; j < num; ++j)
         p = p->next;
+    //삭제할 노드가 head 노드이면 head를 다음 노드로 변경
     if (p == head) {
         head = head->next;
         free(p);
     } else {
+        //삭제할 노드의 이전 노드를 다음 노드로 변경
         struct Tel *q = head;
         while (q->next != p)
             q = q->next;
         q->next = p->next;
         free(p);
     }
+    //data.txt에 저장
     save(head);
 }
 
+//리스트 전체 출력
 void printAll(Tel *head) {
     Tel *p = head;
     int n = 1;
+    //리스트의 끝까지 이동하면서 출력
     while (p != NULL) {
         printf("%d %s %s %s\n",n, p->name, p->tel, p->memo);
         p = p->next;
@@ -170,7 +193,7 @@ int main(int argc, char *argv[]) {
     // 옵션과 문자가 없는 경우 help.txt 출력
     if (--argc == 0) {
         char buf[MAX];
-        FILE *fp = fopen(help, "r");
+        FILE *fp = fopen(help, "rt");
         while (fgets(buf, MAX, fp) != NULL)
             printf("%s", buf);
         fclose(fp);
